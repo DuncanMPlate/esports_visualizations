5e578bb30b0c1ef8814e95ee928be79adaa9cfc8
Samuel Caldwell <samuelcaldwell97@gmail.com>
2021-09-08 20:25:54 -0500

    pushing endpoints

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import Column, Integer, String, Float
 from sqlalchemy.orm import Session
 import os
+import geojson
 from hiddenConfig import password
 
 app=Flask(__name__)
@@ -11,6 +12,7 @@ app=Flask(__name__)
 connection_url=f'postgresql://postgres:{password}@localhost:5432/esports_db'
 # connection_url = os.environ.get('DATABASE_URL').replace('postgres', 'postgresql')
 engine = create_engine(connection_url)
+morerecords = os.path.join(os.getcwd(), "Resources", "map_test.geojson")
 
 # ##### Setting up DB #####
 # Base=declarative_base()
@@ -39,6 +41,13 @@ def fetch_records():
             one_row.append(each_column)
         return_data.append(one_row)
     return jsonify(return_data)
+
+@app.route('/choropleth')
+def choro():
+    with open(morerecords) as f:
+        gj = geojson.load(f)
+    features = gj['features'][0]
+    return(features)
 
 if __name__=='__main__': 
     app.run()
