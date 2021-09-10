c8c64d7281b15202fc4c475562a720319a2364a5
JosiahR89 <82723815+JosiahR89@users.noreply.github.com>
2021-09-10 18:28:03 -0500

    working trace colors

--- static/tournaments.js
+++ static/tournaments.js
@@ -17,25 +17,117 @@ d3.csv('../static/HistoricalEsportData.csv').then(fulfilled_response=>{
   var all_tournaments=fulfilled_response.map(tournament=>tournament['Game'])
   console.log(all_tournaments.filter(onlyUnique).length)
 
-  var dota_tournaments=fulfilled_response.filter(tournament=>tournament['Game']=='Dota 2')
-  console.log(dota_tournaments)
-  var trace1={
+  var SCBW_tournaments=fulfilled_response.filter(tournament=>tournament['Game']=='StarCraft: Brood War')
+  console.log(SCBW_tournaments)
+  var WC3_tournaments=fulfilled_response.filter(tournament=>tournament['Game']=='WarCraft III')
+  console.log(WC3_tournaments)
+  var SSBM_tournaments=fulfilled_response.filter(tournament=>tournament['Game']=='Super Smash Bros. Melee')
+  console.log(SSBM_tournaments)
+  var AoE2_tournaments=fulfilled_response.filter(tournament=>tournament['Game']=='Age of Empires II')
+  console.log(AoE2_tournaments)
+  var CS_tournaments=fulfilled_response.filter(tournament=>tournament['Game']=='Counter-Strike')
+  console.log(CS_tournaments)
+  var SC2_tournaments=fulfilled_response.filter(tournament=>tournament['Game']=='StarCraft II')
+  console.log(SC2_tournaments)
+  var LOL_tournaments=fulfilled_response.filter(tournament=>tournament['Game']=='League of Legends')
+  console.log(LOL_tournaments)
+  var Dota2_tournaments=fulfilled_response.filter(tournament=>tournament['Game']=='Dota 2')
+  console.log(Dota2_tournaments)
+  var CSGO_tournaments=fulfilled_response.filter(tournament=>tournament['Game']=='Counter-Strike: Global Offensive')
+  console.log(CSGO_tournaments)
+  var FN_tournaments=fulfilled_response.filter(tournament=>tournament['Game']=='Fortnite')
+  console.log(FN_tournaments)
+
+  var SCBW_tournaments={
     x: fulfilled_response.map(tournament=>tournament['Date']), 
     y: fulfilled_response.map(tournament=>parseFloat(tournament['Earnings'])),
     text: fulfilled_response.map(tournament=>tournament['Game']),
-    name: 'StarCraft: Brood War'
-    type: 'scatter', 
+    name: 'StarCraft: Brood War',
+    type: 'scatter',
     mode: 'markers'
   }
-  var trace={
+  var WC3_tournaments={
     x: fulfilled_response.map(tournament=>tournament['Date']), 
-    y: fulfilled_response.map(tournament=>parseFloat(tournament['Earnings'])), 
+    y: fulfilled_response.map(tournament=>parseFloat(tournament['Earnings'])),
     text: fulfilled_response.map(tournament=>tournament['Game']),
-    type: 'scatter', 
+    name: 'WarCraft III',
+    type: 'scatter',
+    mode: 'markers'
+  }
+  var SSBM_tournaments={
+    x: fulfilled_response.map(tournament=>tournament['Date']), 
+    y: fulfilled_response.map(tournament=>parseFloat(tournament['Earnings'])),
+    text: fulfilled_response.map(tournament=>tournament['Game']),
+    name: 'Super Smash Bros. Melee',
+    type: 'scatter',
+    mode: 'markers'
+  }
+  var AoE2_tournaments={
+    x: fulfilled_response.map(tournament=>tournament['Date']), 
+    y: fulfilled_response.map(tournament=>parseFloat(tournament['Earnings'])),
+    text: fulfilled_response.map(tournament=>tournament['Game']),
+    name: 'Age of Empires II',
+    type: 'scatter',
+    mode: 'markers'
+  }
+  var CS_tournaments={
+    x: fulfilled_response.map(tournament=>tournament['Date']), 
+    y: fulfilled_response.map(tournament=>parseFloat(tournament['Earnings'])),
+    text: fulfilled_response.map(tournament=>tournament['Game']),
+    name: 'Counter-Strike',
+    type: 'scatter',
+    mode: 'markers'
+  }
+  var SC2_tournaments={
+    x: fulfilled_response.map(tournament=>tournament['Date']), 
+    y: fulfilled_response.map(tournament=>parseFloat(tournament['Earnings'])),
+    text: fulfilled_response.map(tournament=>tournament['Game']),
+    name: 'StarCraft II',
+    type: 'scatter',
+    mode: 'markers'
+  }
+  var LOL_tournaments={
+    x: fulfilled_response.map(tournament=>tournament['Date']), 
+    y: fulfilled_response.map(tournament=>parseFloat(tournament['Earnings'])),
+    text: fulfilled_response.map(tournament=>tournament['Game']),
+    name: 'League of Legends',
+    type: 'scatter',
+    mode: 'markers'
+  }
+  var Dota2_tournaments={
+    x: fulfilled_response.map(tournament=>tournament['Date']), 
+    y: fulfilled_response.map(tournament=>parseFloat(tournament['Earnings'])),
+    text: fulfilled_response.map(tournament=>tournament['Game']),
+    name: 'Dota 2',
+    type: 'scatter',
+    mode: 'markers'
+  }
+  var CSGO_tournaments={
+    x: fulfilled_response.map(tournament=>tournament['Date']), 
+    y: fulfilled_response.map(tournament=>parseFloat(tournament['Earnings'])),
+    text: fulfilled_response.map(tournament=>tournament['Game']),
+    name: 'Counter-Strike: Global Offensive',
+    type: 'scatter',
+    mode: 'markers'
+  }
+  var FN_tournaments={
+    x: fulfilled_response.map(tournament=>tournament['Date']), 
+    y: fulfilled_response.map(tournament=>parseFloat(tournament['Earnings'])),
+    text: fulfilled_response.map(tournament=>tournament['Game']),
+    name: 'Fortnite',
+    type: 'scatter',
     mode: 'markers'
   }
 
-  Plotly.newPlot('myDiv', [trace])
+  // var trace={
+  //   x: fulfilled_response.map(tournament=>tournament['Date']), 
+  //   y: fulfilled_response.map(tournament=>parseFloat(tournament['Earnings'])), 
+  //   text: fulfilled_response.map(tournament=>tournament['Game']),
+  //   type: 'scatter', 
+  //   mode: 'markers'
+  // }
+
+  Plotly.newPlot('myDiv', [SCBW_tournaments, WC3_tournaments, SSBM_tournaments, AoE2_tournaments, CS_tournaments, SC2_tournaments, LOL_tournaments, Dota2_tournaments, CSGO_tournaments, FN_tournaments])
 })
 // StarCraft: Brood War                215
 // WarCraft III                        198
